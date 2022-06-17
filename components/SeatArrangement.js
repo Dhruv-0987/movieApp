83356143b56a522b3ba3fd7c7c8094d63a9a8283
dhruv-0987 <dhruvmat1998@gmail.com>
2022-06-17 13:56:54 +0530

    seat selection with styling changes added with flatlist

--- components/SeatArrangement.js
+++ components/SeatArrangement.js
@@ -4,27 +4,52 @@ import tw from 'twrnc'
 import { useSelector } from 'react-redux'
 import { getNumberOfSeats } from '../slices/bookingSlice'
 const SeatArrangement = ({seats}) => {
+    // to display seats
     const [seatArray, setSeatArray] = useState([])
     const totalSeats = useSelector(getNumberOfSeats)
+    // for all seat numbers
     const [selectedSeats, setSelectedSeats] = useState([])
     const [canSelect, setCanSelect] = useState(true)
-    var allSeats = []
-
+    // contain seat numbers to change classes
+    const [currentSeat, setCurrentSeat] = useState(null)
+    const [unselectedSeat, setUnSelectedSeat] = useState(-1)
     useEffect(()=>{
-        const createSeatArray = async () => {
-            await generateSeatArray()
-        }
-        createSeatArray()
-    },[])
+        const allSeats = generateSeatArray()
+        setSeatArray(allSeats)
+    },[selectedSeats])
 
-    const generateSeatArray = async () =>{
-        const temp = []
+    function generateSeatArray(){
+        console.log('generation seats')
+        var allSeats = []
         for (let i = 1; i <= seats.totalSeats; i++){
-            temp.push({
-                value: i
-            })
+            if (checkSeatBooked(i)){
+                allSeats.push({
+                    value: i,
+                    style: styles.booked,
+                    selected: false
+                })
+            }else if(checkSeatSelected(i)){
+                allSeats.push({
+                    value: i,
+                    style: styles.selected,
+                    selected: false
+                })
+            }
+            else if (checkUnselected(i)) {
+                allSeats.push({
+                    value: i,
+                    style: styles.basic,
+                    selected: false
+                })
+            }else{
+                allSeats.push({
+                    value: i,
+                    style: styles.basic,
+                    selected: false
+                })
+            }
         }
-        setSeatArray(temp)
+        return allSeats
     }
 
     function checkSeatBooked(seatNum){
@@ -32,11 +57,42 @@ const SeatArrangement = ({seats}) => {
     }
 
     function checkSeatSelected(seatNum){
-        return selectedSeats.includes(seatNum) 
+        return currentSeat === seatNum
     }
 
+    function checkUnselected(seatNum){
+        return unselectedSeat === seatNum
+    }
+
+    const handleSeatSelection = (seat) => {
+        if (seat.selected){
+            seat.selected = !seat.selected
+            seat.style = seat.selected ? styles.selected : styles.basic
+            var selected = removeItem(selectedSeats, seat.value)
+            setSelectedSeats(selected)
+            setUnSelectedSeat(seat.value)
+        }else if (!seat.selected){
+            var selected = selectedSeats
+            selected.push(seat.value)
+            setSelectedSeats(selected)
+            seat.selected = !seat.selected
+            seat.style = seat.selected ? styles.selected : styles.basic
+            setCurrentSeat(seat.value)
+        }   
+    }
+
+    function removeItem(array, item) {
+        var i = array.length;
     
-    console.log('seat array',seatArray)
+        while (i--) {
+            if (array[i] === item) {
+                array.splice(array.indexOf(item), 1);
+            }
+        }
+        return array
+    }
+
+    console.log(selectedSeats, currentSeat)
   return (
     <View style={tw`m-2 flex items-center justify-center`}>
     
@@ -46,13 +102,13 @@ const SeatArrangement = ({seats}) => {
             data={seatArray}
             horizontal={false}
             numColumns={12}
-            keyExtractor={(item)=>item.item}
+            keyExtractor={(item)=>item.value}
             renderItem={(item) => {
                 return(
                     <View style={tw`flex-row`}>
-                        <View style={tw`${data.item.value % 6 === 0 ? 'mr-2' : ''}`}>
-                            <TouchableOpacity
-                            style={styles.basic}>
+                        <View style={tw`${item.item.value % 6 === 0 ? 'mr-2' : ''}`}>
+                            <TouchableOpacity   style={item.item.style} onPress={()=>handleSeatSelection(item.item)}
+                            >
                                 <Text style={tw`text-center`}>{item.item.value}</Text>
                             </TouchableOpacity>
                         </View>
@@ -70,26 +126,26 @@ export default SeatArrangement
 
 const styles = StyleSheet.create({
     basic: {
-        margin: '0.25rem', 
-        width: '1.5rem', 
-        height: '1rem',
-        borderWidth: '1px',
+        margin: 2, 
+        width: 25, 
+        height: 20,
+        borderWidth: 1,
         borderColor: '#A7F3D0',
     },
     booked:{
         backgroundColor: '#9CA3AF',
-        margin: '0.25rem', 
-        width: '1.5rem', 
-        height: '1rem',
-        borderWidth: '1px',
+        margin: 2, 
+        width: 25, 
+        height: 20,
+        borderWidth: 1,
         borderColor: '#A7F3D0'
     },
     selected: {
         backgroundColor: '#34D399',
-        margin: '0.25rem', 
-        width: '1.5rem', 
-        height: '1rem',
-        borderWidth: '1px',
+        margin: 2, 
+        width: 25, 
+        height: 20,
+        borderWidth: 1,
         borderColor: '#A7F3D0'
     }
 })
